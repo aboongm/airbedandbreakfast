673c4d40eca8fad843b7db5c791b8ba0f385afff
aboongm <aboongm@yahoo.com>
2023-01-08 09:18:17 +0530

    Add close button functionality for modal

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -2,10 +2,16 @@ import { Controller } from '@hotwired/stimulus';
 import { enter, leave } from 'el-transition';
 
 export default class extends Controller {
+  static targets = ['closeButton'];
   connect() {
     document
       .getElementById('modal-wrapper')
       .addEventListener('click', this.closeModal);
+    this.closeButtonTarget.addEventListener('click', () => {
+      leave(document.getElementById('modal-wrapper'));
+      leave(document.getElementById('modal-backdrop'));
+      leave(document.getElementById('modal-panel'));
+    });
   }
 
   closeModal(event) {
