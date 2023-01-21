43b64f3b64c75d3cb0bc64c33a50bf36ddd68f5b
aboongm <aboongm@yahoo.com>
2023-01-21 14:53:40 +0530

    Unpin lodash and pin lodash-es and update geolocation_controller.js

--- app/javascript/controllers/geolocation_controller.js
+++ app/javascript/controllers/geolocation_controller.js
@@ -1,17 +1,39 @@
 import { Controller } from '@hotwired/stimulus';
 import { getDistance, convertDistance } from 'geolib';
+import { isEmpty } from 'lodash-es';
 
 export default class extends Controller {
   static targets = ['property'];
   connect() {
-    window.navigator.geolocation.getCurrentPosition((pos) => {
-      console.log('geolib: ', getDistance);
-      this.element.dataset.latitude = pos.coords.latitude;
-      this.element.dataset.longitude = pos.coords.longitude;
+    if (
+      isEmpty(this.element.dataset.latitude) &&
+      isEmpty(this.element.dataset.longitude)
+    ) {
+      window.navigator.geolocation.getCurrentPosition((pos) => {
+        this.element.dataset.latitude = pos.coords.latitude;
+        this.element.dataset.longitude = pos.coords.longitude;
 
+        this.propertyTargets.forEach((propertyTarget) => {
+          let distanceFrom = getDistance(
+            { latitude: pos.coords.latitude, longitude: pos.coords.longitude },
+            {
+              latitude: propertyTarget.dataset.latitude,
+              longitude: propertyTarget.dataset.longitude,
+            }
+          );
+          let distance = convertDistance(distanceFrom, 'km');
+          propertyTarget.querySelector(
+            '[data-distance-away]'
+          ).innerHTML = `${Math.ceil(distance)} kms away`;
+        });
+      });
+    } else {
       this.propertyTargets.forEach((propertyTarget) => {
         let distanceFrom = getDistance(
-          { latitude: pos.coords.latitude, longitude: pos.coords.longitude },
+          {
+            latitude: this.element.dataset.latitude,
+            longitude: this.element.dataset.longitude,
+          },
           {
             latitude: propertyTarget.dataset.latitude,
             longitude: propertyTarget.dataset.longitude,
@@ -22,6 +44,6 @@ export default class extends Controller {
           '[data-distance-away]'
         ).innerHTML = `${Math.ceil(distance)} kms away`;
       });
-    });
+    }
   }
 }
