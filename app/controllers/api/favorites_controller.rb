01ca6c5094d59742418fd17219e5f9bf1dae0594
aboongm <aboongm@yahoo.com>
2023-01-22 15:31:25 +0530

    Add like and unlike functionalities hitting the api endpoints

--- app/controllers/api/favorites_controller.rb
+++ app/controllers/api/favorites_controller.rb
@@ -1,5 +1,6 @@
 module Api
     class FavoritesController < ApplicationController
+        protect_from_forgery with: :null_session
         before_action :authenticate_user!
 
         def create
--- app/javascript/controllers/favorites_controller.js
+++ app/javascript/controllers/favorites_controller.js
@@ -1,14 +1,52 @@
 import { Controller } from '@hotwired/stimulus';
+import axios from 'axios';
 
 export default class extends Controller {
   favorite() {
     console.log('favorite button clicked!');
-    if (this.element.dataset.favorited === 'true') {
+    if (this.element.dataset.userLoggedIn === 'false') {
+      document.querySelector('[data-header-target="userAuthLink"]').click();
+    } else if (this.element.dataset.favorited === 'true') {
       this.element.dataset.favorited = 'false';
       this.element.setAttribute('fill', 'lightgray');
+
+      axios
+        .delete(
+          this.element.dataset.unfavoritedUrl,
+          {
+            id: this.element.dataset.favoriteId,
+          },
+          {
+            headers: {
+              ACCEPT: 'application/json',
+            },
+          }
+        )
+        .then((response) => {
+          this.element.dataset.favorited = 'false';
+          this.element.setAttribute('fill', 'lightgray');
+        });
     } else {
-      this.element.setAttribute('fill', 'red');
-      this.element.dataset.favorited = 'true';
+      // this.element.dataset.favorited = 'true';
+      // this.element.setAttribute('fill', 'red');
+
+      axios
+        .post(this.element.dataset.favoriteUrl, {
+          params: {
+            user_id: this.element.dataset.userId,
+            property_id: this.element.dataset.propertyId,
+          },
+          headers: {
+            ACCEPT: 'application/json',
+          },
+        })
+        .then((response) => {
+          this.element.dataset.favorited = 'true';
+          this.element.setAttribute('fill', 'red');
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
   }
 }
